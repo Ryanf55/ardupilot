c2fa41a80bae0f9879dcab23802894d8346ff8f8
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-08-09 20:56:27 -0600

    Copter: Clarify set_velocity_and_yaw_rate frames and units
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- ArduCopter/AP_ExternalControl_Copter.h
+++ ArduCopter/AP_ExternalControl_Copter.h
@@ -8,8 +8,9 @@
 class AP_ExternalControl_Copter : public AP_ExternalControl {
 public:
     /*
-      set linear velocity and yaw rate. Pass NaN for yaw_rate_rads to not control yaw
-      velocity is in earth frame, NED, m/s
+      Set linear velocity and yaw rate. Pass NaN for yaw_rate_rads to not control yaw.
+      Velocity is in earth frame, NED [m/s].
+      Yaw is in earth frame, NED [rad/s].
      */
     bool set_linear_velocity_and_yaw_rate(const Vector3f &linear_velocity, float yaw_rate_rads) override;
 };
